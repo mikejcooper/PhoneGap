5d0828c6541c85d1b0933db6f49458adc703edf8
Michael Cooper <mikejcooper1@gmail.com>
2015-06-28 18:19:05 +0100

    update

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers','ui.bootstrap'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -28,6 +28,16 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     abstract: true,
     templateUrl: "templates/menu.html",
     controller: 'AppCtrl'
+  })
+
+  .state('app.home', {
+    url: "/home",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/home.html",
+        controller: 'HomeCtrl'
+      }
+    }
   })
 
   .state('app.search', {
@@ -57,15 +67,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     })
 
-  .state('app.single', {
-    url: "/playlists/:playlistId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/playlists');
 });
--- js/controllers.js
+++ js/controllers.js
@@ -1,44 +1,30 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers',['ui.bootstrap'])
+
+.controller('CollapseCtrl', function ($scope) {
+  $scope.isCollapsed = true;
+})
+
+    //.controller('linkCtrl', function ($scope, params ) {
+    //  $scope.changeLink = document.getElementById("test2").href="www.google.com";
+    //})
+
+
+
+
+
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  
+
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-  
-  // Form data for the login modal
-  $scope.loginData = {};
+  //
+  //// Form data for the login modal
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
 })
 
 .controller('PlaylistsCtrl', function($scope) {
@@ -53,4 +39,54 @@ angular.module('starter.controllers', [])
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
+})
+
+.controller('HomeCtrl', function($scope) {
+      console.log("hello2");
+  $scope.cards = [
+    { title: 'Bristol Freshers', avatarImg:"events/CCL.png" , coverImg:"events/4.png", description:"First event of term!", location: "Embargos, Bristol", venue:"The Fleece", date: "Sunday 27th September", day:"Sunday", id: 1, coverURL: "https://www.facebook.com/events/971419459536809/", avatarURL: "https://www.facebook.com/groups/521020464709928/?fref=ts"},
+    { title: 'Bristol Freshers', avatarImg:"events/CCL.png" , coverImg:"events/4.png", description:"First event of term!", location: "Frogmore Street, BS1 5NA", venue:"The Fleece", date: "Sunday 27th September", day:"Saturday", id: 1 },
+  ];
+      //Device selection for Maps integration.
+      $scope.changeLink = function (location){
+        var devicePlatform = device.platform;
+        var mapRoot = location + ", UK";
+        console.log(devicePlatform);
+        if(devicePlatform == "iOS1"){
+          mapRoot = "maps:q=" + mapRoot;
+        }
+        else if (devicePlatform == 'Android'){
+          mapRoot = "geo:0,0?q=" + mapRoot
+        }
+        else {
+          mapRoot = "https://www.google.co.uk/maps/search/" + mapRoot
+        }
+        document.getElementById("maps").href = window.open(mapRoot, '_system');
+        document.getElementById("maps2").href = window.open(mapRoot, '_system');
+        return false;
+    };
+
+      $scope.isCollapsed = true;
+
+      //Add to config.xml for appAvailability plugin ->>> <gap:plugin name="com.ohh2ahh.plugins.appavailability" />
+
+      $scope.checkApp = function(URL, id){
+        //var scheme  = 'fb://';
+        //appAvailability.check(scheme,       // URI Scheme or Package Name
+        //    function() {  // Success callback - application installed
+        //      document.getElementById(id.toString()).href = "fb://" + URLNative;
+        //    },
+        //    function() {  // Error callback
+        //      document.getElementById(id.toString()).href = URL;
+        //    })
+
+        document.getElementById(id.toString()).href = window.open(URL, '_system');
+        return false;
+      };
+})
+
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
 });
+
+
